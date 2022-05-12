d71a6221f37666502c0bd0f109e0ce67c6fa647d
BaTmAnOmOrE <callanhpinto1995@gmail.com>
2022-05-12 14:55:04 +1200

    whack_a_mole completed

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,33 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
+//get a random cell
+//mole image appears in a random cell
+//click on mole
+//mole reappears in another cell
 
-console.log("Whack-a-Mole!")
+var cells = document.getElementsByTagName('td')
+
+var randomIndex = Math.floor(Math.random() * cells.length)
+
+var randomCell = cells[randomIndex]
+
+var mole = document.createElement('img')
+
+mole.src = './mole.PNG'
+
+mole.id = 'mole'
+
+randomCell.appendChild(mole)
+
+mole.onclick = whackedMole
+
+function whackedMole() {
+  //   for (let i = 0; i < randomCell.length; i++) {
+  //     randomCell[i].onclick = 'TD'
+  randomIndex = Math.floor(Math.random() * cells.length)
+  randomCell = cells[randomIndex]
+  randomCell.appendChild(mole)
+
+  var audio = new Audio('./whack-audio.wav')
+  audio.play()
+}
